3a46fbbfceb6e2bf3fe18c1a2ab1ef79b239f986
Nitin-Gahlawat <101346709+Nitin-Gahlawat@users.noreply.github.com>
2023-07-14 17:51:27 +0530

    Implementation of searching algorithm
    In this file we are having Implementation of searching algorithm like linear search and binary search.

---
+++ Data-Structures/iterative_search.java
@@ -0,0 +1,49 @@
+import java.util.Arrays ;
+/**
+ * This class contain the implementation of searching algorithm.
+ */
+class search{
+    /**
+     * Perform a linear search on a array
+     *
+     * @param arr The element array for the searching.
+     * @param ele The element which we need to search.
+     */
+    int linearsearch(int arr[],int ele){
+        for(int i=0;i<arr.length;i++)
+            if(arr[i]==ele)
+                return i;
+        return -1;
+    }
+    /**
+     * Perform a binary search on a array
+     *
+     * @param arr The element array for the searching.
+     * @param ele The element which we need to search.
+     * 
+     * **NOTE** For applying the binary search we need to sort the array.
+     */
+    int binarysearch(int[] arr,int ele){    //arr is pass by reference
+        Arrays.sort(arr);   //binarysearch works on a sorted array hence sorting is necessary
+        int beg=0,end=arr.length,mid;
+        while(beg<end){
+            mid=(beg+end)/2;
+            if(arr[mid]==ele)
+                return mid;
+            else if(ele>arr[mid])
+                beg=mid ;
+            else
+                end=mid;
+        }
+        return -1;
+    }
+}
+
+class iterative_search {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        int ele=4;
+        search obj=new search();
+        System.out.println(obj.linearsearch(arr,ele));
+    }
+}
