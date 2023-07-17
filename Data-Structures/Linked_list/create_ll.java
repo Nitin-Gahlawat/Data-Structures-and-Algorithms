8d7d8cdbace499f517754e9c6fa365ba851fdaf1
Nitin-Gahlawat <gahlawatnitin03@gmail.com>
2023-07-17 21:15:06 +0530

    Adding Linked list to the Data-Structure

---
+++ Data-Structures/Linked_list/create_ll.java
@@ -0,0 +1,81 @@
+/**
+ * This class represents an the node of the linked list;
+ *
+ */
+class node {
+    int data;
+    node link;
+}
+
+/**
+ * This class represents a linked list.
+ */
+class linkedlist {
+    node head;
+
+    /**
+     * Initializes a new instance of the linkedlist class with the given element.
+     *
+     * @param value initially the value to be inserted
+     */
+    linkedlist(int value) {
+        // initial construction with one value
+        this.head = new node();
+        head.data = value;
+        head.link = null;
+    }
+
+    /**
+     * Initializes a new instance of the linkedlist class.
+     *
+     */
+    linkedlist() {
+    }
+
+    /**
+     * Prints all the elements in the linked list.
+     */
+    void print() {
+        node temp = head;
+        while (temp != null) {
+            System.out.printf("%d ", temp.data);
+            temp = temp.link;
+        }
+        System.out.printf("\n");
+    }
+
+    /**
+     * Inserts an element at the end of the linked list.
+     *
+     * @param value the value to be inserted
+     */
+    void addelement(int value) {
+
+        // creating a new node
+        node obj = new node();
+        obj.data = value;
+        obj.link = null;
+
+        if (head == null) {
+            this.head = new node();
+            head = obj;
+            return;
+        }
+        node temp = head;
+        while (temp.link != null) {
+            temp = temp.link;
+        }
+        temp.link = obj;
+        return;
+    }
+}
+
+class create_ll {
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+        list.addelement(8);
+        list.addelement(18);
+        list.addelement(28);
+        list.print();
+    }
+}
