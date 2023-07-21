64caf388d6a2bbf060c882f83c792b149d83929c
Nitin-Gahlawat <gahlawatnitin03@gmail.com>
2023-07-21 23:05:18 +0530

    delete_value function is added for the deleting the given element form the linked list in the file delete_ll.java

--- Data-Structures/Linked_list/delete_ll.java
+++ Data-Structures/Linked_list/delete_ll.java
@@ -72,15 +72,19 @@ class linkedlist {
         return;
     }
 
+    /**
+     * Delete the element from the end of the linked list.
+     *
+     */
     void delete_end() {
         node temp = head;
-        if(temp==null){
+        if (temp == null) {
             System.out.println("No element found in the linked list");
             return;
         }
         size--;
-        if(temp.link==null){
-            temp=null;
+        if (temp.link == null) {
+            temp = null;
             return;
         }
         while (temp.link.link != null) {
@@ -89,11 +93,49 @@ class linkedlist {
         temp.link = null;
     }
 
+    /**
+     * Delete the element from the beginning of the linked list
+     *
+     */
     void delete_beg() {
         if (head != null)
             head = head.link;
         else
             System.out.println("No element found in the linked list");
+    }
+
+    /**
+     * Delete the given element from the linked list
+     *
+     * @param value the value to be Deleted
+     */
+    void delete_value(int value) {
+        // check for the head if it is empty or not
+        if (head == null) {
+            System.out.println("The linked lsit is empty");
+            return;
+        }
+        node temp = head;
+        node pre = head;
+        // for the first node in the linked list
+        if (temp.data == value) {
+            head = temp.link;
+            return;
+        }
+        // for all the other node in the linked list expect for the first node
+        while (temp.data != value && temp != null)
+
+        {
+            pre = temp;
+            temp = temp.link;
+        }
+        if (temp == null)
+            System.out.println("the element not found");
+        else {
+            pre.link = temp.link;
+
+        }
+
     }
 }
 
@@ -101,10 +143,10 @@ class delete_ll {
     public static void main(String[] args) {
         linkedlist list = new linkedlist();
         list.insert(18);
-        list.insert(8);
+        list.insert(45);
         list.insert(28);
-        list.insert(200);
-        // list.delete_beg();
+        list.insert(250);
+        list.delete_value(18);
         list.print();
     }
 }
