113cf7d2f6b52426353768afb029e81040aa3732
Nitin-Gahlawat <gahlawatnitin03@gmail.com>
2023-07-26 23:02:27 +0530

    Adding pop and top function into the stack class.

--- Data-Structures/stack/create.java
+++ Data-Structures/stack/create.java
@@ -9,11 +9,21 @@ class stack {
     // stack implemented using array
     int[] stack_pointer;
 
+    /**
+     * stack constructor
+     *
+     * @param size it is the size of the stack.
+     */
     stack(int size) {
         this.size = size;
         this.stack_pointer = new int[this.size];
     }
 
+    /**
+     * push operation insert a new value in stack at the top.
+     *
+     * @param value element to add the item in the stack .
+     */
     void push(int value) {
         if (top + 1 > this.size) {
             System.out.println("overflow");
@@ -21,6 +31,23 @@ class stack {
         }
         stack_pointer[this.top] = value;
         this.top++;
+    }
+
+    /**
+     * return the top value of the stack.
+     *
+     */
+    int top() {
+        return stack_pointer[this.top - 1];
+    }
+
+    /**
+     * removes the top value from the stack.
+     *
+     */
+    void pop() {
+        stack_pointer[this.top - 1] = 0;
+        this.top--;
     }
 }
 
@@ -34,10 +61,14 @@ public class create {
      * @param args The command-line arguments.
      */
     public static void main(String[] args) {
-        stack obj = new stack(3);
-        obj.push(12);
-        obj.push(1);
-        obj.push(182);
-        obj.push(162);
+        stack st = new stack(4);
+        st.push(12);
+        st.push(1);
+        st.push(182);
+        st.push(162);
+        System.out.println(st.top());
+        st.pop();
+        System.out.println(st.top());
+
     }
 }
